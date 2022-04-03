cef0f8c2f70187db7950deb51eb92ef27ecd0f60
Namık Karabıyık <namik.karabiyik@ddtech.com.tr>
2022-04-03 14:36:50 +0300

    I am rich app

---
+++ tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tutorial
+//
+//  Created by Namık Karabıyık on 2.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
